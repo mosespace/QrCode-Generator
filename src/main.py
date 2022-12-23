1657b2f37a87217104e65fb7594f61fa53611dee
Kisakye Moses <kisakyemoses50@gmail.com>
2022-12-23 17:44:27 +0300

    Final Code in the Main.py

--- src/main.py
+++ src/main.py
@@ -2,10 +2,10 @@ import qrcode
 
 
 I = input("Type in the url which you want to visit: ")  #write your url here
-print("Your QRCode is now Saved!:", I)
+print("Your QRCode is One Step To Be Saved!:", I)  #This messages shows that the QR Code has been downloaded ans saved
 
 
-def generate(data, img_name="qrCode.png"):
+def generate(data, img_name=(input('Enter Desired Filename With Extension, e.g. qrCode.png: '))):
 
     """_summary_
 
